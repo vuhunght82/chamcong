8f0bce65e03eded8cad0a86bfb1ce33d9894f266
vuhunght82 <74271126+vuhunght82@users.noreply.github.com>
2025-11-08 13:59:45 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+<!-- firebase-config.js -->
+// Cấu hình Firebase của bạn
+ const firebaseConfig = {
+  apiKey: "AIzaSyAb5AZOBvEVY3a_9ioEKTW-wuVMXdnRZGQ",
+  authDomain: "sellglass-3ebe9.firebaseapp.com",
+  databaseURL: "https://sellglass-3ebe9-default-rtdb.asia-southeast1.firebasedatabase.app", // <- sửa lại
+  projectId: "sellglass-3ebe9",
+  storageBucket: "sellglass-3ebe9.appspot.com",
+  messagingSenderId: "513741829035",
+  appId: "1:513741829035:web:1b2c59dcf1a0be329358ee"
+};
+
+// Chỉ khởi tạo 1 lần
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.database();
